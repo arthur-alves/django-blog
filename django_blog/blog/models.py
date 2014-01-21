95ff59a4adf229b1dae9fb9858dea7be97ab3ec8
Arthur <arthur.4lves@gmail.com>
2014-01-21 11:16:57 -0200

    Adiciona arquivos iniciais.

---
+++ django_blog/blog/models.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+
+class Category(models.Model):
+    category = models.CharField(
+        u'Categorias', primary_key=True, max_length=255
+    )
+
+    class Meta:
+        verbose_name = u'Categoria'
+        verbose_name_plural = u'Categorias'
+
+    def __unicode__(self):
+        return self.category
+
+
+class Posts(models.Model):
+    owner = models.ForeignKey(
+        User, related_name='entries', verbose_name='Criador')
+    title = models.CharField(u'Título', max_length=255)
+    wording = models.TextField(u'Texto')
+    tags = models.CharField(max_length=255)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(
+        u'Data de Publicação', auto_now_add=True)
+    published = models.BooleanField(u'Publicar?', default=False)
+
+    class Meta:
+        verbose_name = u'Postagem'
+        verbose_name_plural = u'Postagens'
+
+    def __unicode__(self):
+        return self.title
