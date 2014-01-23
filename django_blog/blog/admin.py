41ce2dea7fc142bf430465c992fceabfab18adcd
Arthur <arthur.4lves@gmail.com>
2014-01-23 17:16:35 -0200

    Cria novas colunas no admin

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from blog.models import Posts, Category
 
 # Register your models here.
-admin.site.register(Posts)
+class PostClass(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'published')
+
+
+admin.site.register(Posts, PostClass)
 admin.site.register(Category)
