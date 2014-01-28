a263caac2036016f05444ba79a6d08a7da554651
Arthur <arthur.4lves@gmail.com>
2014-01-28 13:35:59 -0200

    Adiciona config do settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blogdb',
         'USER': 'blogadmin',
-        'PASSWORD': 'arthur*777',
+        'PASSWORD': '123*777',
         'HOST': 'localhost'
     }
 }
