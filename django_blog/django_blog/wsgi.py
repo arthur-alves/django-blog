363c76d0e50b74c5a7742b830c354155ad26ecf5
Arthur Alves <arthur@asha.(none)>
2014-01-28 09:58:30 -0500

    Adiciona configuração do django

--- django_blog/django_blog/wsgi.py
+++ django_blog/django_blog/wsgi.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os, sys
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
-if PROJECT_ROOT not in sys.path:
-    sys.path.append(PROJECT_ROOT)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_module")
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+# settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+print PROJECT_ROOT
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
