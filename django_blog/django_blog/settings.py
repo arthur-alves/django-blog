c30afc8de0e2716e2907b42bb8a3b88f48a8f5b5
Arthur <arthur.4lves@gmail.com>
2014-01-28 08:55:30 -0200

    Adiciona correção para servidor

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'south',
     'django_extensions',
     'blog',
+    'gunicorn',
 
 )
 
@@ -83,4 +84,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = BASE_DIR + 'static'
 STATIC_URL = '/static/'
